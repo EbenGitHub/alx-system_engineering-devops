a931a165b675902e2a655b48e29ec3b461589b5a
EbenGitHub <abenezergoo@gmail.com>
2022-10-17 18:18:51 +0300

    first api proj

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,40 +1,30 @@
 #!/usr/bin/python3
-""" This will use REST API for a given employee ID and displays TODO list in progress
+"""For a given employee ID, returns information about
+their TODO list progress"""
 
-Usage: 
-    $ python3 0-gather_data_from_an_API.py <employee ID>
-
-"""
-
-import json
 import requests
 import sys
 
 if __name__ == "__main__":
 
-    userID = sys.argv[1]
-    userID = int(userID)
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
 
-    user = requests.get(f'https://jsonplaceholder.typicode.com/users/{userID}')
-    user = json.loads(user.content.decode())
+    name = user.json().get('name')
 
-    todo = requests.get(f'https://jsonplaceholder.typicode.com/todos/')
-    todo = json.loads(todo.content.decode())
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    totalTasks = 0
+    completed = 0
 
-    todo_list = []
-    task_comp = 0
-    task_ttl = 0
+    for task in todos.json():
+        if task.get('userId') == int(userId):
+            totalTasks += 1
+            if task.get('completed'):
+                completed += 1
 
-    for users in todo:
-        if (users['userId'] == userID):
-            task_ttl += 1
-            if (users['completed'] == True):
-                todo_list.append(users['title'])
-                task_comp += 1
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, completed, totalTasks))
 
-    user_todo = f"""Employee {user['name']} is done with tasks({task_comp}/{task_ttl}):"""
-
-    print(user_todo, end="")
-
-    for todos in todo_list:
-        print(f"\n\t {todos}", end="")
+    print('\n'.join(["\t " + task.get('title') for task in todos.json()
+          if task.get('userId') == int(userId) and task.get('completed')]))
