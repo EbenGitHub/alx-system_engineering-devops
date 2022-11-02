001101f870b573314b00f9f8a3b0e6bb54ec0549
Eben@ysh <77492217+EbenGitHub@users.noreply.github.com>
2022-11-02 23:08:46 +0300

    recursing until the pagination ends

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,8 +6,9 @@ import requests
 def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    UserAgent = "linux:alx-sysEng.adv_api.ubuntu Ebenezer"
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": UserAgent
     }
     params = {
         "after": after,
