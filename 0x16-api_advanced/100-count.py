4e963fac044040137fecbd0f4507b4cc034b16e9
EbenGitHub <abenezergoo@gmail.com>
2022-11-02 20:29:07 +0000

    lower words and recures to find their cnt til pag ends

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -14,8 +14,9 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
         count (int): The parameter of results matched thus far.
     """
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    UserAgent = "linux:alx-sysEng.adv_api.ubuntu Ebenezer"
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": UserAgent
     }
     params = {
         "after": after,
@@ -38,7 +39,8 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
     for c in results.get("children"):
         title = c.get("data").get("title").lower().split()
         for word in word_list:
-            if word.lower() in title:
+            word = word.lower()
+            if word in title:
                 times = len([t for t in title if t == word.lower()])
                 if instances.get(word) is None:
                     instances[word] = times
